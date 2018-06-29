5b05c544dba9401c22f005611bc2b451efb344c0
Struggler <endlessrepeating1986@gmail.com>
2018-06-29 21:14:25 +1000

    proj1b: Added 'a' and 'B' case.

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -13,6 +13,7 @@ public class TestOffByOne {
         assertTrue( offByOne.equalChars('a', 'b'));
 
         assertTrue( offByOne.equalChars('A', '@'));
+        assertTrue( offByOne.equalChars('A', 'B'));
         assertTrue( offByOne.equalChars('1', '2'));
         assertTrue( offByOne.equalChars('2', '1'));
 
@@ -22,6 +23,7 @@ public class TestOffByOne {
         assertFalse( offByOne.equalChars('z', 'a'));
         assertFalse( offByOne.equalChars('a', 'a'));
         assertFalse( offByOne.equalChars('a', 'A'));
+        assertFalse( offByOne.equalChars('a', 'B'));
     }
 
 }
