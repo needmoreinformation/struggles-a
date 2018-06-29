a0d67f67ffcec76be02ac1c7310b3d9f851feac9
Struggler <endlessrepeating1986@gmail.com>
2018-06-29 22:07:20 +1000

    hw2: Initial commit.

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,126 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private static final int[][] NEIGHBOURS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private WeightedQuickUnionUF grid2;
+    private WeightedQuickUnionUF grid;
+    private int topSiteDummyVal;
+    private int bottomSiteDummyVal;
+    private int gridSize; /* Grid dimension */
+    private int numOpenSites;
+    private boolean[][] gridState;
+
+    /**
+     * Create n-by-n grid, with all sites blocked. Must take time proportional to n^2.
+     * @param n
+     */
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be greater than zero.");
+        }
+        numOpenSites = 0;
+        gridSize = n;
+        grid = new WeightedQuickUnionUF(n * n + 2);
+        grid2 = new WeightedQuickUnionUF(n * n + 1);
+        topSiteDummyVal = 0;
+        bottomSiteDummyVal = n * n + 1;
+        gridState = new boolean[n][n];
+    }
+
+    private int xyTo1D(int row, int col) {
+        return (row) * gridSize + col;
+    }
+
+    private boolean isCoordsOutOfBounds(int row, int col) {
+        return (row < 0 || row >= gridSize || col < 0 || col >= gridSize);
+    }
+
+    /**
+     * Open site (row, col) if it is not open already.
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        if (isCoordsOutOfBounds(row, col)) {
+            throw new IllegalArgumentException("Coordinates are out of bounds: (" + row + ", " + col + ")");
+        }
+
+        if (!gridState[row][col]) {
+            gridState[row ][col] = true;
+            numOpenSites++;
+            for (int[] neighbour : NEIGHBOURS) {
+                int newRow = row + neighbour[0];
+                int newCol = col + neighbour[1];
+
+                if (newRow < 0) {
+                    grid.union(xyTo1D(row, col), topSiteDummyVal);
+                    grid2.union(xyTo1D(row, col), topSiteDummyVal);
+                } else if (newRow >= gridSize) {
+                    grid.union(xyTo1D(row, col), bottomSiteDummyVal);
+                } else {
+                    if (!isCoordsOutOfBounds(newRow, newCol) && isOpen(newRow, newCol)) {
+                        grid.union(xyTo1D(newRow, newCol), xyTo1D(row, col));
+                        grid2.union(xyTo1D(newRow, newCol), xyTo1D(row, col));
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    /**
+     * Is site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        if (isCoordsOutOfBounds(row, col)) {
+            throw new IllegalArgumentException("Coordinates are out of bounds: (" + row + ", " + col + ")");
+        }
+
+        return gridState[row][col];
+    }
+
+    /**
+     * Is site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        if (isCoordsOutOfBounds(row, col)) {
+            throw new IllegalArgumentException("Coordinates are out of bounds: (" + row + ", " + col + ")");
+        }
+        return isOpen(row, col) && grid2.connected(xyTo1D(row, col), topSiteDummyVal);
+    }
+
+    /**
+     * Number of open sites.
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return numOpenSites;
+    }
+
+    /**
+     * Does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return grid.connected(topSiteDummyVal, bottomSiteDummyVal);
+    }
+
+    /**
+     * Test client (optional).
+     *
+     * Usage: PercolationStats n T
+     * @param args
+     */
+    public static void main(String[] args) {
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,100 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
+    private int numTrials;
+    private double numTrialsSqrt;
+    private double[] percolationThreshold;
+    private double percMean;
+    private double percStddev;
+
+    /**
+     * Perform trials independant of experiments on an n-by-n grid
+     * @param n
+     * @param trials
+     */
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("n and trials must be above 0");
+        }
+
+        numTrials = trials;
+        numTrialsSqrt = Math.sqrt(trials);
+        int numSites = n * n;
+        percolationThreshold = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                p.open(row, col);
+            }
+            percolationThreshold[i] = (double)p.numberOfOpenSites() / numSites;
+        }
+        percMean = StdStats.mean(percolationThreshold);
+        percStddev = StdStats.stddev(percolationThreshold);
+    }
+
+    /**
+     * Sample mean of percolation threshold.
+     * @return
+     */
+    public double mean() {
+        return percMean;
+    }
+
+    /**
+     * Sample standard deviation of percolation threshold.
+     * @return
+     */
+    public double stddev() {
+        return percStddev;
+    }
+
+    private double confidenceHelper() {
+        return 1.96 * stddev();
+    }
+
+    /**
+     * Low endpoint of 95% confidence interval.
+     * @return
+     */
+    public double confidenceLo() {
+        return mean() - (confidenceHelper() / numTrialsSqrt);
+    }
+
+    /**
+     * High endpoint of 95% confidence interval.
+     * @return
+     */
+    public double confidenceHi() {
+        return mean() + (confidenceHelper() / numTrialsSqrt);
+    }
+
+    /**
+     * Test client.
+     *
+     * Usage: PercolationStats n T
+     * Example: PercolationStats 200 100
+     *
+     * Limitations:
+     * n > 0
+     * T > 0
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int gridSize = Integer.parseInt(args[0]);
+        int numTrials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(gridSize, numTrials);
+        StdOut.printf("%-23s = %.16f\n", "mean", ps.mean());
+        StdOut.printf("%-23s = %.16f\n", "stddev", ps.stddev());
+        StdOut.printf("%-23s = [%.16f, %.16f]\n", "95% confidence interval", ps.confidenceLo(), ps.confidenceHi());
+
+    }
 }
