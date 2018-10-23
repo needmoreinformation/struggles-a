cceaa45fcfed745b2a875433c4205726b220f710
Struggler <endlessrepeating1986@gmail.com>
2018-10-23 12:56:58 +1000

    hw3: Done.

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,17 +1,28 @@
 package hw3.hash;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
-         * Write a utility function that returns true if the given oomages
-         * have hashCodes that would distribute them fairly evenly across
-         * M buckets. To do this, convert each oomage's hashcode in the
-         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
-         * and ensure that no bucket has fewer than N / 50
-         * Oomages and no bucket has more than N / 2.5 Oomages.
-         */
-        return false;
+        Map<Integer, Integer> bucketCounts = new HashMap<>();
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() % 0x7FFFFFF) % M;
+            bucketCounts.putIfAbsent(bucketNum, 0);
+            bucketCounts.put(bucketNum, bucketCounts.get(bucketNum) + 1);
+        }
+
+        int N = oomages.size();
+        double lowerBound = N / 50;
+        double higherBound = N / 2.5;
+        for (int bucketNum : bucketCounts.keySet()) {
+            int count = bucketCounts.get(bucketNum);
+            if (count < lowerBound || count > higherBound) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -1,15 +1,14 @@
 package hw3.hash;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 
 import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 
 public class TestSimpleOomage {
@@ -25,10 +24,26 @@ public class TestSimpleOomage {
 
     @Test
     public void testHashCodePerfect() {
-        /* TODO: Write a test that ensures the hashCode is perfect,
-          meaning no two SimpleOomages should EVER have the same
-          hashCode UNLESS they have the same red, blue, and green values!
-         */
+        SimpleOomage so = null;
+        Set<Integer> hashCodes = new HashSet<>();
+
+        int step = 5;
+        for (int red = 0; red < 256; red += step) {
+            for (int blue = 0; blue < 256; blue += step) {
+                for (int green = 0; green < 256; green += step) {
+                    so = new SimpleOomage(red, green, blue);
+                    if (hashCodes.contains(so.hashCode())) {
+                        fail("Hash code for (red = " + red + " green = " + green +
+                                " blue = " + blue + ") already exists - is not perfect.");
+                    } else {
+                        hashCodes.add(so.hashCode());
+                    }
+                }
+            }
+        }
+
+        so = new SimpleOomage(25, 25, 25);
+        assertTrue("Hash code isn't perfect", hashCodes.contains(so.hashCode()));
     }
 
     @Test
@@ -42,7 +57,7 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -50,10 +65,9 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
-    /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -63,7 +77,7 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
